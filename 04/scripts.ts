c9c1f512998659d0141e427af7c53a74f2f72e74
Wendel Veloso <142932695+wendelveloso@users.noreply.github.com>
2024-05-03 10:48:27 -0300

    Exercise Finished

---
+++ 04/scripts.ts
@@ -0,0 +1,22 @@
+const geradorDeEtiquetas = (info: {
+  produto: string;
+  lote: number;
+  ano: number;
+  qtd: number;
+}):string[] => {
+  let resultado: string[] = []
+
+  for (let i = 0; i <= info.qtd; i++) {
+    resultado.push(`${info.lote}-${info.ano}-${i.toString().padStart(3, "0")}`);
+  }
+  return resultado;
+};
+
+console.log(
+  geradorDeEtiquetas({
+    produto: "CPU Dual Core 3.0GHZ",
+    lote: 321,
+    ano: 2022,
+    qtd: 5,
+  })
+);
